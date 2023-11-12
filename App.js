47fc1f14b50d0cc62c6d33f59d63a79194d14010
TranQuangHien <tqhien27102002@gmail.com>
2023-11-12 15:20:00 +0700

    hoan thanh con thieu delete

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Screen01 from './component/Screen01';
+import Screen02 from './component/Screen02';
+import Screen03 from './component/Screen03';
+import SignUp from "./component/SignUp";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Screen01'>
+        <Stack.Screen
+          name='Screen01' component={Screen01}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name='Screen02' component={Screen02}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name='Screen03' component={Screen03}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name='SignUp'
+          options={{headerShown: false}}
+          component={SignUp}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
